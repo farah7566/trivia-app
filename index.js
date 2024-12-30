b9bf34e8c833dfe903adfe738488c9284d6f409e
farah7566 <farahjaouadi9@gmail.com>
2024-12-30 15:25:24 +0100

    troubleshooting error

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ app.get('/trivia', async (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://0.0.0.0:${PORT}`);
+  console.log(`Server running on http://0.0.0.0${PORT}`);
 });
